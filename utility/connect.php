509ff9da7aff3c4f8718b8a911b0e64cd58b8512
Tolgahan ACAR <47753083+tolgahanacar@users.noreply.github.com>
2021-06-16 16:17:42 +0300

    Update connect.php
    setAttribute added for database

--- utility/connect.php
+++ utility/connect.php
@@ -1,6 +1,7 @@
 <?php
 try{
     $db = new PDO("mysql:host=localhost;dbname=seourl;charset=UTF8","tolga","123456");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $ErrorMessage){
     echo $ErrorMessage->getMessage();
     die();
